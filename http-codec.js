c3dd5afcab6dfeda93861921b3c348011ddd8c33
Tim Caswell <tim@creationix.com>
2013-10-07 11:47:44 -0500

    Save changes to new interface

--- http-codec.js
+++ http-codec.js
@@ -1,144 +1,232 @@
-"use strict";
-
-var bops = require('bops');
-
-function syntaxError(message, array) {
-  return new SyntaxError(message + ": " +
-    JSON.stringify(bops.to(bops.from(array)))
-  );
-}
-
-var states = {
-  method: function (byte, data, emit) {
-    // Capital letter
-    if (byte > 0x40 && byte <= 0x5a) {
-      data.push(byte);
-      return "method";
-    }
-    // Space
-    if (byte === 0x20) {
-      data.method = bops.to(bops.from(data));
-      data.length = 0;
-      return "path";
-    }
-    data.push(byte);
-    emit(syntaxError("Invalid Method", data));
-    return "error";
-  },
-  path: function (byte, data, emit) {
-    if (byte === 0x20) {
-      data.path = bops.to(bops.from(data));
-      data.length = 0;
-      return "version";
-    }
-    if (byte === 0x0d || byte === 0x0a) {
-      data.push(byte);
-      emit(syntaxError("Unexpected newline in path", data));
-      return "error";
-    }
-    data.push(byte);
-    return "path";
-  },
-  version: function (byte, data, emit) {
-    if (byte === 0x0d) {
-      var match = bops.to(bops.from(data)).match(/HTTP\/(1).([01])/);
-      if (!match) {
-        emit(syntaxError("Invalid HTTP version string", data));
-        return "error";
-      }
-      data.version = [parseInt(match[1], 10), parseInt(match[2], 10)];
-      data.length = 0;
-      return "endhead";
-    }
-    data.push(byte);
-    return "version";
-  },
-  endhead: function (byte, data, emit) {
-    if (byte === 0x0a) {
-      data.headers = [];
-      return "key";
-    }
-    emit(new SyntaxError("Syntax Error in newline after HTTP request header"));
-  },
-  key: function (byte, data, emit) {
-    if (byte === 0x0d) {
-      if (data.length === 0) {
-        return "endheaders";
-      }
-      emit(new SyntaxError("Unexpected newline"));
-      return "error";
-    }
-    if (byte === 0x3a) {
-      data.headers.push(bops.to(bops.from(data)));
-      data.length = 0;
-      return "value";
-    }
-    data.push(byte);
-    return "key";
-  },
-  value: function (byte, data) {
-    if (byte === 0x0d) {
-      data.headers.push(bops.to(bops.from(data)));
-      data.length = 0;
-      return "endheader";
-    }
-    if (byte === 0x20 && data.length === 0) {
-      // Ignore leading spaces in header values
-      return "value";
-    }
-    data.push(byte);
-    return "value";
-  },
-  endheader: function (byte, data, emit) {
-    if (byte === 0x0a) {
-      return "key";
-    }
-    emit(new SyntaxError("Invalid line termination"));
-    return "error";
-  },
-  endheaders: function (byte, data, emit) {
-    if (byte === 0x0a) {
-      emit(null, {
-        method: data.method,
-        path: data.path,
-        version: data.version,
-        headers: data.headers,
-      });
-      return "body";
-    }
-    emit(new SyntaxError("Invalid head termination"));
-    return "error";
-  },
-  error: function (byte, data, emit) {
-    emit();
-    return "error";
-  },
-  body: function (byte, data) {
-    data.push(byte);
-    return "body";
-  }
+var bops, HTTP1_1;
+module.exports = function (platform) {
+  bops = platform.bops;
+  HTTP1_1 = bops.from("HTTP/1.1");
+  return {
+    server: {
+      encoder: serverEncoder,
+      decoder: serverDecoder,
+    },
+    client: {
+      encoder: clientEncoder,
+      decoder: clientDecoder,
+    },
+  };
 };
 
-exports.decoder = decoder;
-function decoder(emit) {
+function serverEncoder(write) {
+  return function (res) {
+    throw "TODO: Implement serverEncoder";
+  };
+}
 
-  var state = "method";
-  var data = [];
+function clientEncoder(write) {
+  return function (req) {
+    if (req === undefined) return write(undefined);
+    if (bops.is(req)) return write(req);
+    var head = req.method + " " + req.path + " HTTP/1.1\r\n";
+    req.headers.forEach(function (pair) {
+      head += pair[0] + ": " + pair[1] + "\r\n";
+    });
+    head += "\r\n";
+    write(bops.from(head));
+  };
+}
 
-  var fn = function (err, chunk) {
-    if (chunk === undefined) return emit(err);
-    for (var i = 0, l = chunk.length; i < l; i++) {
-      state = states[state](chunk[i], data, emit);
-    }
-    if (state === "body" && data.length) {
-      emit(null, bops.from(data));
-      data.length = 0;
+function clientDecoder(emit) {
+  return parser(true, emit);
+}
+
+function serverDecoder(emit) {
+  return parser(false, emit);
+}
+
+function parser(client, emit) {
+  var position = 0, code = 0;
+  var key = "", value = "";
+  var chunked = false, length;
+  var headers = [];
+  var $start = client ? $client : $server;
+  var state = $start;
+  return function (chunk) {
+    if (chunk === undefined) return emit();
+    if (!state) return emit(chunk);
+    var i = 0, length = chunk.length;
+    while (i < length) {
+      state = state(chunk[i++]);
+      if (state) continue;
+      emit(bops.subarray(chunk, i));
+      break;
     }
   };
-  fn.is = "min-stream-write";
-  return fn;
+
+  function $client(byte) {
+    if (byte === HTTP1_1[position++]) return $client;
+    if (byte === 0x20 && position === 9) {
+      position = 0;
+      return $code;
+    }
+    throw new SyntaxError("Must be HTTP/1.1 response");
+  }
+
+  function $code(byte) {
+    if (byte === 0x20) return $message;
+    if (position++ < 3) {
+      code = (code * 10) + byte - 0x30;
+      position = 0;
+      return $code;
+    }
+    throw new SyntaxError("Invalid status code");
+  }
+
+  function $message(byte) {
+    if (byte === 0x0d) {
+      position = 0;
+      return $newline;
+    }
+    return $message;
+  }
+
+  function $server(byte) {
+    throw "TODO: Implement server-side parser";
+  }
+
+  function $newline(byte) {
+    if (byte === 0x0a) return $end;
+    throw new SyntaxError("Invalid line ending");
+  }
+
+  function $end(byte) {
+    if (byte === 0x0d) return $ending;
+    return $key(byte);
+  }
+
+  function $key(byte) {
+    if (byte === 0x3a) return $sep;
+    key += String.fromCharCode(byte);
+    return $key;
+  }
+
+  function $sep(byte) {
+    if (byte === 0x20) return $sep;
+    return $value(byte);
+  }
+
+  function $value(byte) {
+    if (byte === 0x0d) {
+      var lower = key.toLowerCase();
+      if (lower === "transfer-encoding" && value === "chunked") {
+        chunked = true;
+      }
+      else if (lower === "content-length") length = parseInt(value, 10);
+      headers.push([key, value]);
+      key = "";
+      value = "";
+      return $newline;
+    }
+    value += String.fromCharCode(byte);
+    return $value;
+  }
+
+  function $ending(byte) {
+    if (byte === 0x0a) {
+      emit({
+        code: code,
+        headers: headers
+      });
+      headers = [];
+      code = 0;
+      if (chunked) return chunkMachine(emit, $start);
+      return null;
+    }
+    throw new SyntaxError("Invalid header ending");
+  }
+
 }
-decoder.is = "min-stream-push-filter";
+
+function chunkMachine(emit, $start) {
+  var position = 0, size = 0;
+  var chunk = null;
+  return $len;
+  function $len(byte) {
+    if (byte === 0x0d) return $chunkStart;
+    size <<= 4;
+    if (byte >= 0x30 && byte < 0x40) size += byte - 0x30;
+    else if (byte > 0x60 && byte <= 0x66) size += byte - 0x57;
+    else if (byte > 0x40 && byte <= 0x46) size += byte - 0x37;
+    else throw new SyntaxError("Invalid chunked encoding length header");
+    return $len;
+  }
+
+  function $chunkStart(byte) {
+    if (byte === 0x0a) {
+      if (size) {
+        chunk = bops.create(size);
+        return $chunk;
+      }
+      return $ending;
+    }
+    throw new SyntaxError("Invalid chunk ending");
+  }
+
+  function $chunk(byte) {
+    chunk[position++] = byte;
+    if (position < size) return $chunk;
+    return $ending;
+  }
+
+  function $ending(byte) {
+    if (byte !== 0x0d) throw new SyntaxError("Problem in chunked encoding");
+    return $end;
+
+  }
+
+  function $end(byte) {
+    if (byte !== 0x0a) throw new SyntaxError("Problem in chunked encoding");
+    var next;
+    if (size) {
+      emit(chunk);
+      next = $len;
+    }
+    else {
+      emit();
+      next = $start;
+    }
+    chunk = null;
+    size = 0;
+    position = 0;
+    return next;
+  }
+
+}
+
+
+// exports.encoder = encoder;
+// function encoder(emit) {
+//   var fn = function (err, item) {
+//     if (item === undefined) return emit(err);
+//     if (typeof item === "string") {
+//       return emit(null, bops.from(item));
+//     }
+//     if (bops.is(item)) {
+//       return emit(null, item);
+//     }
+//     var head = "HTTP/1.1 " + item.statusCode + " " + STATUS_CODES[item.statusCode] + "\r\n";
+//     for (var i = 0, l = item.headers.length; i < l; i += 2) {
+//       head += item.headers[i] + ": " + item.headers[i + 1] + "\r\n";
+//     }
+//     head += "\r\n";
+//     emit(null, bops.from(head));
+//   };
+//   fn.is = "min-stream-write";
+//   return fn;
+// }
+// encoder.is = "min-stream-push-filter";
+// function syntaxError(message, array) {
+//   return new SyntaxError(message + ": " +
+//     JSON.stringify(bops.to(bops.from(array)))
+//   );
+// }
 
 var STATUS_CODES = {
   '100': 'Continue',
@@ -194,26 +282,3 @@ var STATUS_CODES = {
   '509': 'Bandwidth Limit Exceeded',
   '510': 'Not Extended'                // RFC 2774
 };
-
-
-exports.encoder = encoder;
-function encoder(emit) {
-  var fn = function (err, item) {
-    if (item === undefined) return emit(err);
-    if (typeof item === "string") {
-      return emit(null, bops.from(item));
-    }
-    if (bops.is(item)) {
-      return emit(null, item);
-    }
-    var head = "HTTP/1.1 " + item.statusCode + " " + STATUS_CODES[item.statusCode] + "\r\n";
-    for (var i = 0, l = item.headers.length; i < l; i += 2) {
-      head += item.headers[i] + ": " + item.headers[i + 1] + "\r\n";
-    }
-    head += "\r\n";
-    emit(null, bops.from(head));
-  };
-  fn.is = "min-stream-write";
-  return fn;
-}
-encoder.is = "min-stream-push-filter";
